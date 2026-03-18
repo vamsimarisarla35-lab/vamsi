a2937d3bcf3652c68d207d34a31ee578a7ff22f6
vamsimarisarla35 <vamsimarisarla35@gmail.com>
2026-03-18 14:08:45 +0530

    Day2 Learning

---
+++ day 2.py
@@ -0,0 +1,84 @@
+'''
+variables -->variable is basically named stroge location that it is used to data in the memory to make it simple it is the lable with points out to a value -->stroage placeholders
+
+ruules for defining variables
+--->AtoZ,atoz,0to9'''
+
+'''#store your personal details
+name = "vamsi"
+age = 20
+location = "bobbli"
+email_id = "marisarlavamsi2000@gmail.com "
+print(name,age,location,email_id)
+
+
+#assin same values to multipul variables
+x=y=z=11
+print(x,y,z)
+
+
+value = 1+3j
+print(value)
+print(type(value))
+
+j3 = 25
+value = 2+j3
+print(value)
+print(type(value))
+
+
+#type casting  ---> converting one type to another
+age = 20
+print(type(age))
+
+b = float(age)
+print(b)
+print(type(b))
+
+c = complex(age)
+print(c)
+print(type(c))'''
+
+
+
+#input --->input()/ output --->print()
+a = 5
+print(a)
+
+
+a = input("eanter na value")
+
+
+
+#details of the student
+name = input("eanter the student name:")
+print("----------")
+admistion_fees = int(input("enter the admistion fees:")
+tution_fees = float(input("enter the tution fees:")
+hostle_fees = float(input("enter the hostle fees:")
+#calcilate the total fees
+total fees : (admistion_fees + tution_fees +hostle_fees )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
