6af2c096a9e009b3cc94e1589642449337094fdb
vamsimarisarla35 <vamsimarisarla35@gmail.com>
2026-03-20 12:41:19 +0530

    work in operators

---
+++ day4.py
@@ -0,0 +1,231 @@
+'''
+operator --->an operator is a symbal that perform an operator on one or more values (operands) and [produces a result
+
+
+operators are primary used :
+--->calcilate value
+--->compare value / inputs
+--->make decions
+--->contral the program flow
+
+there are major seven categaries of operations in python
+
+--->arthimatic operators
+--->assignment operators
+--->comparision operators
+--->membership operators (in,not in)
+--->identity operaters(is,is not)
+--->bitwisew operators
+--->logical operators(and,or,not)
+'''
+
+#arhimatic operatipons -->arthimatic operations perform
+#  +--.addition ,_ -->substraction,*-->multipilication, /-->division
+#** --> exponent ,%-->modulus ,//-->integer division
+'''
+
+a=5
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)  #retun the value is flote
+print(a**b)#retun the exponental value
+print(a%b)#retun reminder
+print(a//b)#retun quatient'''
+
+
+
+'''num = int(imput("enter the first value:"))
+num = float(input("enter the second value"))
+result = (num1+num2)*4
+print("the result is ",result)
+'''
+'''#assignment operation
+#-->assign ,+=addition assignment.,
+#-= -->subtract assignment,*=,/=,%=,//=,**=
+
+
+a=4
+b=3
+a +=2#-->a=a+2
+print(a)
+b +=a
+print(b)
+
+b -= 3#b= b-3
+print(b)
+b*=a#b=b*a
+print(b)'''
+
+
+
+
+'''#relation or comparision operation -->they always retun the boolen values (true/false)
+
+#==is eaqul to ,!=not equal to
+#  ,less than ,>grater than,>=,<=
+
+
+a = int(input("enter the value "))
+b = int(input("enter the another value"))         
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)'''
+
+
+'''#membership operator -->ythey check for the existnce of an object in a
+#collection -->in,not in
+
+a ="codegnan"
+print(type(a))
+print('a'in a)
+print('z' in 'vamsi')
+print('z' in 'codegnan')
+'''
+
+
+
+'''#and,or,not
+
+
+age =  int(input("enter the age"))
+vote_right = 18'''
+
+#(id) function it is different from == operation ->is,is not
+
+
+'''a = [1,2,3]
+b = [1,2,3]
+print(a==b)
+print(id(a))
+print(id(b))
+
+c = b
+print(c)
+print(id(c))
+print(c is b)'''
+        
+
+#bitwise operations -->bitwise AND & bitwise or |perfome bitwise operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
