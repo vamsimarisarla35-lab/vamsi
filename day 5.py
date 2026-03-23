b0577517e5a6cc066287e550ce7025d0187ece0a
vamsimarisarla35 <vamsimarisarla35@gmail.com>
2026-03-23 13:11:32 +0530

    conditions in python

---
+++ day 5.py
@@ -0,0 +1,105 @@
+'''
+if statement
+if-else statement
+if-elif-else statement
+code Q
+'''
+
+age = int(input("enter your age"))
+if age >=18:
+    print("your ageis 18 or above")
+
+    
+
+
+
+student_att =int(input("pls enter your sem attendence:"))
+if student_att >=75:
+    print("you can directly sit in the exam")
+
+
+
+age: int(input("enter your age"))
+if age >=18:
+    print ("you can vote ")
+
+else :
+    print(f"you can not and have to wait {18-age}")
+
+'''
+
+'''
+total_am = int(input("enter the total shoping money:"))
+if total_am>=100:
+    print("no delevery cost")
+
+else:
+    print(f"add{100-total_am}to your cart")'''
+
+'''#if-elif-else statement in the elif part ,i can cheek one
+#more conditionstudent_marks =int(input("enter your marks :"))
+'''            "you got a+ grade"
+'''            
+elif student_marks >=75 and student_marks <90:
+    print("you got a grade ")
+
+elif students_marks >=60 and students_marks  <75:
+    print("you got b grade ")
+    
+else:
+    print ("your fail")'''
+
+
+'''
+
+
+num_1 = int(input("enter the first value"))
+num_2 = int(input("enter  the second value"))
+user_choise = int(input("enter your choice:"))
+if user_choice =="+":
+    print (num_1+num_2)
+elif user_choice =="-":
+
+      print(num_1-num_2)      
+
+
+num_1 = int(input("enter 1st number"))
+num_2 = int(input("enter 2nd number"))
+
+
+
+user_choice = int(input("pls enter any number:"))
+if user_choice %2 == 0:
+    print(f"{user-choice}is a user number ")
+
+else:
+    print(f"{user_choice}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
