0a03ee62567c5d25eb22e1042f83030e03f40f39
vamsimarisarla35 <vamsimarisarla35@gmail.com>
2026-04-30 12:36:17 +0530

    day 26

---
+++ day 26.py
@@ -0,0 +1,77 @@
+
+'''
+import matplotlib.pyplot as plt
+plt.bar([2024,2025,2026],[67,89,50])
+plt.title("car sales")
+plt.x-lable("years")
+plt.y-lable("number of cars sold")
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+cars = ["BMW", "Audi", "Tesla", "Toyota", "Hyundai"]
+sales = [50, 70, 40, 90, 60]
+plt.bar(cars, sales)
+plt.xlabel("Car Brands")
+plt.ylabel("Sales")
+plt.title("Car Sales Bar Graph")
+plt.show()
+           
+import matplotlib.pyplot as plt
+plt.scatter([1,2,3,4],[200,400,100,800], color="Yellow", s=100)
+plt.title("Swift Car Sales")
+plt.xlabel("Years")
+plt.ylabel("Number of Sales")
+plt.show()
+
+import matplotlib.pyplot as plt
+plt.bar([2024,2025,2026],[67,89,50], color="Gray", s=150)
+plt.title("Car Sales")
+plt.xlabel("Years")
+plt.ylabel("Number of cars sold")
+plt.show()
+
+
+import matplotlib.pyplot as plt
+plt.bar([2024,2025,2026],[67,89,50])
+plt.title("Car Sales")
+plt.xlabel("Years")
+plt.ylabel("Number of Cars sold")
+plt.show()
+
+import matplotlib.pyplot as plt
+plt.scatter([1,2,3,4],[200,400,100,800], color="Yellow", s=100)
+plt.title("Swift Car Sales")
+plt.xlabel("Years")
+plt.ylabel("Number of Sales")
+plt.show()
+
+
+import matplotlib.pyplot as plt
+plt.bar([2024,2025,2026],[67,89,50], 150, color="Gray")
+plt.title("Car Sales")
+plt.xlabel("Years")
+plt.ylabel("Number of cars sold")
+plt.show()
+'''
+
+import matplotlib.pyplot as plt
+values = [40, 15, 35, 20]
+labels = ["Backend (Python)","Frontend (HTML, CSS)","Framework (Flask)","Library (NumPy, Pandas, Matplotlib)"]
+plt.pie(values, labels=labels)
+plt.title("ATM Application (Project)")
+plt.legend(["Sai", "Niral", "Abdul", "Teja"])
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
